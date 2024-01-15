162bc5fea6c644ee19054e8187f3ebedc512806a
ZIDOL2 <149359158+ZIDOL2@users.noreply.github.com>
2024-01-16 01:25:22 +0900

    테스트 classpath랑 gitignore 커밋해도 괜찮은걸까?

---
+++ Java_Practice/src/com/zidol/programmers/test/Base01.java
@@ -0,0 +1,31 @@
+package com.zidol.programmers.test;
+
+import java.util.Arrays;
+
+public class Base01 {
+	
+	//문자열 정수형으로 바꾸기
+	public int solution(String n_str) {
+		int answer = 0;
+        answer = Integer.parseInt(n_str);
+        return answer;
+    }
+	
+	//카운트 업
+	public int[] solution2(int start_num, int end_num) {
+		//배열의 길이
+        int size = end_num-start_num+1;
+        
+        //배열 선언(배열의 길이는 size)
+        int[] answer = new int[size];
+        
+        //반복문으로 배열 채우기
+        for(int i=0; i<size; i++){
+            answer[i] = start_num++;
+        }
+        //확인
+        System.out.println(Arrays.toString(answer));
+        
+        return answer;
+    }
+}
---
+++ Java_Practice/src/com/zidol/programmers/test/run/Run.java
@@ -0,0 +1,15 @@
+package com.zidol.programmers.test.run;
+
+import com.zidol.programmers.test.Base01;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		Base01 b = new Base01();
+		
+		//b.solution("10");
+		b.solution2(3, 10);
+	}
+
+}
