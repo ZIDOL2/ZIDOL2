60d29e338ef1ae0ed7688a083a23259a373d17d6
ZIDOL2 <@DESKTOP-JISU.mshome.net>
2024-11-04 20:52:10 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'javastudy' of https://github.com/ZIDOL2/ZIDOL2.git into
    javastudy

--- Java_Practice/src/com/zidol/baekjoon/level1/AmulB2.java
+++ Java_Practice/src/com/zidol/baekjoon/level1/AmulB2.java
@@ -2,7 +2,7 @@ package com.zidol.baekjoon.level1;
 
 import java.util.Scanner;
 
-public class AmulB {
+public class AmulB2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int A, B;
