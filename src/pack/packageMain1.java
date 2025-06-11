fe49d49f7a162edf4e5f160772dd448b680cbec3
ZIDOL2 <149359158+ZIDOL2@users.noreply.github.com>
2025-06-11 10:10:56 +0900

    커밋 다시

--- src/pack/packageMain1.java
+++ src/pack/packageMain1.java
@@ -7,6 +7,7 @@ public class packageMain1 {
     public static void main(String[] args) {
         Data data = new Data();
 
+        // import 안하려면 full name 사용해야함
 //        pack.a.User user = new pack.a.User();
         User user = new User();
     }
